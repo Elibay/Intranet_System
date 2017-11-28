c98248512d732732e8ec7cd8021057ff5d66f2bc
Elibay <elibay98@gmail.com>
2017-11-28 14:45:28 +0600

    Intranet done 80%

---
+++ Project/Intranet/src/Users/Employees/Employee.java
@@ -0,0 +1,17 @@
+package Users.Employees;
+import Users.*;
+
+public class Employee extends User {
+	protected int salary;
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public Employee(String name, String surname, int id, int salary, String type) {
+		super (name, surname, id, type);
+		this.salary = salary;
+	}
+	
+}
